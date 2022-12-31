9955b2103860d962ef9636eef6fe83c25a5c5712
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-12-31 14:01:37 +0800

    Add module manage application, donation, and user

---
+++ app/Filament/Resources/ApplicationResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ApplicationResource\Pages;
+use App\Filament\Resources\ApplicationResource\RelationManagers;
+use App\Models\Application;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ApplicationResource extends Resource
+{
+    protected static ?string $model = Application::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('student_id')
+                    ->required(),
+                Forms\Components\TextInput::make('amount')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('approved_at'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('student_id'),
+                Tables\Columns\TextColumn::make('amount'),
+                Tables\Columns\TextColumn::make('approved_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListApplications::route('/'),
+            'create' => Pages\CreateApplication::route('/create'),
+            'edit' => Pages\EditApplication::route('/{record}/edit'),
+        ];
+    }    
+}
