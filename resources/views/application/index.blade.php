206b0d866843078d4f1da77cc47fd4d9dd84c797
izzat syamir <112875861+izzatsymr@users.noreply.github.com>
2023-01-12 22:52:11 +0800

    Done application controller and index view

--- resources/views/application/index.blade.php
+++ resources/views/application/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-8 lg:px-8">
             <x-partials.card>
                 <div class="mb-5 mt-4">
                     <div class="flex flex-wrap justify-between">
@@ -22,91 +22,141 @@
                                         </button>
                                     </div>
                                 </div>
-                            </form>
+                            </form>  
                         </div>
                         <div class="md:w-1/2 text-right">
-                            {{-- @can('create', App\Models\Role::class)
-                                <a href="{{ route('roles.create') }}" class="button button-primary">
-                                    <i class="mr-1 icon ion-md-add"></i>
-                                    @lang('crud.common.create')
-                                </a>
-                            @endcan --}}
-                        </div>
+                            @can('create', App\Models\Application::class)
+                            <a
+                                href="{{ route('applications.create') }}"
+                                class="button button-primary"
+                            >
+                                <i class="mr-1 icon ion-md-add"></i>
+                                @lang('crud.common.create')
+                            </a>
+                            @endcan
+                        </div> 
                     </div>
-                </div>
+                </div> 
 
                 <div class="block w-full overflow-auto scrolling-touch">
                     <table class="w-full max-w-full mb-4 bg-transparent">
                         <thead class="text-gray-700">
-                            <tr>
+                            <tr class="px-4 py-3 text-left">
                                 <th class="px-4 py-3 text-left">
-                                    @lang('crud.roles.inputs.name')
+                                    Student ID
                                 </th>
-                                <th></th>
+                                <th class="px-4 py-3 text-left">
+                                    Amount
+                                </th>
+                                <th class="px-4 py-3 text-left">
+                                    Reason
+                                </th>
+                                <th class="px-4 py-3 text-left">
+                                    Status
+                                </th>
+                                <th class="px-4 py-3 text-left">
+                                    Approved at
+                                </th>
                             </tr>
                         </thead>
                         <tbody class="text-gray-600">
-                            {{-- @forelse($roles as $role)
-                                <tr class="hover:bg-gray-50">
-                                    <td class="px-4 py-3 text-left">
-                                        {{ $role->name ?? '-' }}
-                                    </td>
-                                    <td class="px-4 py-3 text-center" style="width: 134px;">
-                                        <div role="group" aria-label="Row Actions"
-                                            class="
+                            @foreach($applications as $application)
+                            <tr class="hover:bg-gray-50">
+                                <td class="px-4 py-3 text-left">
+                                    {{ $application->student_id ?? 'Student ID' }}
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    {{ $application->amount ?? 'Amount' }}
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    {{ $application->reason ?? 'Reason' }}
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    {{ $application->status ?? 'Status' }}
+                                </td>
+                                <td class="px-4 py-3 text-left">
+                                    {{ $application->approved_at ?? 'Approved' }}
+                                </td>
+                        
+                                <td
+                                    class="px-2 py-3 text-center"
+                                    style="width: 134px;"
+                                >
+                                    <div
+                                        role="group"
+                                        aria-label="Row Actions"
+                                        class="
                                             relative
                                             inline-flex
-                                            align-middle
-                                        ">
-                                            @can('update', $role)
-                                                <a href="{{ route('roles.edit', $role) }}" class="mr-1">
-                                                    <button type="button" class="button">
-                                                        <i class="icon ion-md-create"></i>
-                                                    </button>
-                                                </a>
-                                                @endcan @can('view', $role)
-                                                <a href="{{ route('roles.show', $role) }}" class="mr-1">
-                                                    <button type="button" class="button">
-                                                        <i class="icon ion-md-eye"></i>
-                                                    </button>
-                                                </a>
-                                                @endcan @can('delete', $role)
-                                                <form action="{{ route('roles.destroy', $role) }}" method="POST"
-                                                    onsubmit="return confirm('{{ __('crud.common.are_you_sure') }}')">
-                                                    @csrf @method('DELETE')
-                                                    <button type="submit" class="button">
-                                                        <i
-                                                            class="
+                                            align-left
+                                        "
+                                    >
+                                        @can('update', $applications)
+                                        <a
+                                            href="{{ route('applications.edit', $application) }}"
+                                            class="mr-1"
+                                        >
+                                            <button
+                                                type="button"
+                                                class="button"
+                                            >
+                                                <i
+                                                    class="icon ion-md-create"
+                                                ></i>
+                                            </button>
+                                        </a>
+                                        @endcan @can('view', $application)
+                                        <a
+                                            href="{{ route('applications.show', $application) }}"
+                                            class="mr-1"
+                                        >
+                                            <button
+                                                type="button"
+                                                class="button"
+                                            >
+                                                <i class="icon ion-md-eye"></i>
+                                            </button>
+                                        </a>
+                                        @endcan @can('delete', $application)
+                                        <form
+                                            action="{{ route('applications.destroy', $application) }}"
+                                            method="POST"
+                                            onsubmit="return confirm('{{ __('crud.common.are_you_sure') }}')"
+                                        >
+                                            @csrf @method('DELETE')
+                                            <button
+                                                type="submit"
+                                                class="button"
+                                            >
+                                                <i
+                                                    class="
                                                         icon
                                                         ion-md-trash
                                                         text-red-600
-                                                    "></i>
-                                                    </button>
-                                                </form>
-                                            @endcan
-                                        </div>
-                                    </td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="2">
-                                        @lang('crud.common.no_items_found')
-                                    </td>
-                                </tr>
-                            @endforelse --}}
+                                                    "
+                                                ></i>
+                                            </button>
+                                        </form>
+                                        @endcan
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
                         </tbody>
                         <tfoot>
                             <tr>
                                 <td colspan="2">
                                     <div class="mt-10 px-4">
-                                        {{-- {!! $roles->render() !!} --}}
+                                        {!! $applications->render() !!}
                                     </div>
                                 </td>
                             </tr>
                         </tfoot>
                     </table>
                 </div>
+
             </x-partials.card>
         </div>
     </div>
+    
 </x-app-layout>
